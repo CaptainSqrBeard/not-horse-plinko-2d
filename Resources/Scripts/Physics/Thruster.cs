4ce0f431865c06d5eb6d82a05922f60bc48bfd14
CaptainSqrBeard <iliya.11.levchenko@gmail.com>
2023-08-08 20:38:07 +0600

    a

--- Resources/Scripts/Physics/Thruster.cs
+++ Resources/Scripts/Physics/Thruster.cs
@@ -6,23 +6,18 @@ public partial class Thruster : Node2D
 	[Export] RigidBody2D Body;
 	[Export] float Force = 1000f;
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	public bool State = false;
+
+	public override void _PhysicsProcess(double delta)
 	{
+		if (State)
+		{
+			Body.ApplyForce(new Vector2(0, -Force).Rotated(GlobalRotation));
+		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public void SetState(bool state)
 	{
+		State = state;
 	}
-
-    public void Forward()
-    {
-        Forward(Force);
-    }
-
-    public void Forward(float force)
-    {
-        Body.ApplyForce(new Vector2(0, -force).Rotated(GlobalRotation));
-    }
 }
