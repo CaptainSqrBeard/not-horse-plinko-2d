e46b6fb4798ccddf2f260ec93bc258d83ddbb5c4
maxthetomas <max@maxthetomas.ru>
2023-08-17 00:24:02 +0300

    I made the game better

--- Resources/Scripts/TimerRefresh.cs
+++ Resources/Scripts/TimerRefresh.cs
@@ -3,23 +3,23 @@ using System;
 
 public partial class TimerRefresh : Node
 {
-    [Export] Label SpeedrunTime;
-    [Export] bool RefreshOnReady = true;
+	[Export] Label SpeedrunTime;
+	[Export] bool RefreshOnReady = true;
 
 	SpeedrunManager _speedrunManager;
 	public override void _Ready()
 	{
 		_speedrunManager = GetNode<SpeedrunManager>("/root/SpeedrunManager");
 
-        if (RefreshOnReady)
-        {
-            _speedrunManager.Finish();
-            if (SpeedrunTime != null)
-            {
-                var timePassed = _speedrunManager.EndTime - _speedrunManager.StartTime;
-                var timeSpan = new TimeSpan((long) timePassed * 10);
-                SpeedrunTime.Text = timeSpan.ToString("hh\\:mm\\:ss\\.fff");
-            }
-        }
+		if (RefreshOnReady)
+		{
+			_speedrunManager.Finish();
+			if (SpeedrunTime != null)
+			{
+				var timePassed = _speedrunManager.EndTime - _speedrunManager.StartTime;
+				var timeSpan = new TimeSpan((long) timePassed * 10);
+				SpeedrunTime.Text = timeSpan.ToString("hh\\:mm\\:ss\\.fff");
+			}
+		}
 	}
 }
